5edc9f8be9e69e31c3e21e42eda664ea78452e9e
murugan <murugan@murugans-Mac-mini.local>
2016-11-28 17:55:42 +0530

    Initial Commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by murugan on 28/11/16.
+//  Copyright © 2016 murugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
